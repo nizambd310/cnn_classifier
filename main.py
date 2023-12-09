b3c0bfa5c04607702281899122bfa5dca6b7543f
nizambd310 <uddinahmednizam@gmail.com>
2023-12-09 08:51:50 +0600

    prepare base model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from cnn_classifier_project import logger
 from cnn_classifier_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-# from cnn_classifier_project.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnn_classifier_project.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 # from cnn_classifier_project.pipeline.stage_03_model_training import ModelTrainingPipeline
 # from cnn_classifier_project.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
@@ -21,16 +21,16 @@ except Exception as e:
 
 
 
-# STAGE_NAME = "Prepare base model"
-# try: 
-#    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#    prepare_base_model = PrepareBaseModelTrainingPipeline()
-#    prepare_base_model.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
